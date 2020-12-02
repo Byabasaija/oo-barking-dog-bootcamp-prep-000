b07c5367f815a1eb112f764c2ddd1c51dd5bbc3e
Pascal Byabasaija <basaijapascal9@gmail.com>
2020-12-02 16:48:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Dog
+  getter :name, :bark
+  setter :name, :bark
+  def initialize(name,bark)
+    @name = name
+    @bark = bark
+  end
+
+end
+fido = Dog.new("Fido","Woof")
+fido.name
+fido.bark
